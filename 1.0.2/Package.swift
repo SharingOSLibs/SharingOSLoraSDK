3c25adf0eb1fe5bac630127829e1780931803b0e
GitLionelZhang <lionelzhangz@gmail.com>
2022-02-17 20:37:47 +0800

    [Added]First Commit

---
+++ 1.0.2/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "SharingOSLockSDK",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "SharingOSLockSDK", 
+            targets: ["SharingOSLockSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SharingOSLockSDK", 
+            path: "SharingOSLockSDK.xcframework")
+    ])
